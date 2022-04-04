ebd58a59647f8d0843edde561417041f6e28909f
Richard.Liek <richardliek@gmail.com>
2022-04-03 20:50:38 -0400

    Second commit

---
+++ Week1/Extra Practice/Name.java
@@ -0,0 +1,7 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("My name is Richard Liek");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is in Berwick, ME");
+    }
+}
