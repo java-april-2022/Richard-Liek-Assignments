3d7d26a6e2803a5266962b9af0efe7857cfb949e
Richard.Liek <richardliek@gmail.com>
2022-04-09 18:03:40 -0400

    update

--- Week1/Cafe Business Logic/CafeUtil.java
+++ Week1/Cafe Business Logic/CafeUtil.java
@@ -1,3 +1,56 @@
+import java.util.ArrayList;
+
 public class CafeUtil {
+    /* 
+     Streak goal test!!
+    */
+
+   public int getStreakGoal() {
+       int sum = 0;
+       for(int i = 1; i <=10; i++){
+           sum += i;
+       }
+       return sum;
+   }
     
+   /* 
+   ORDER TOTAL TEST!!
+   */
+
+    public double getOrderTotal(double[] prices) {
+        double totalSum = 0;
+        for (int i = 0; i < prices.length; i++) {
+            totalSum = totalSum + prices[i];
+        }
+        return totalSum;
+    }
+
+    /* 
+    Display Menu Test!!
+    */
+
+    public void displayMenu(ArrayList<String> menuItems){
+        String name = menuItems.get(0); // to access an element in an ArrayList using an index
+        for (int i = 0; i < menuItems.size(); i++)
+            System.out.println(i + name);
+    }
+
+    // Add Customer Test!!
+
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        
+        System.out.printf("Hello, %s! \n", userName);
+
+        System.out.printf("There are currently %s people infront of you.", customers.size());
+        customers.add(userName);
+    }
+
+    // Ninja bonuses!
+
+    public void printPriceChart(String product, double price, int maxQuantity) {
+        System.out.println(product);
+
+    }
 }
--- Week1/Cafe Business Logic/TestCafe.java
+++ Week1/Cafe Business Logic/TestCafe.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 public class TestCafe {
     public static void main(String[] args) {
         
@@ -8,11 +9,13 @@ public class TestCafe {
       of the CafeUtil class. 
       Hint: it will need to correspond with the variable name used below..
     */
+
+        CafeUtil appTest = new CafeUtil(); 
     	
         /* ============ App Test Cases ============= */
     
-        System.out.println("\n----- Streak Goal Test -----");
-        System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
+        // System.out.println("\n----- Streak Goal Test -----");
+        // System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
     
         // System.out.println("----- Order Total Test-----");
         // double[] lineItems = {3.5, 1.5, 4.0, 4.5};
@@ -27,12 +30,14 @@ public class TestCafe {
         // menu.add("mocha");
         // appTest.displayMenu(menu);
     
-        // System.out.println("\n----- Add Customer Test-----");
-        // ArrayList<String> customers = new ArrayList<String>();
-        // // --- Test 4 times ---
-        // for (int i = 0; i < 4; i++) {
-        //     appTest.addCustomer(customers);
-        //     System.out.println("\n");
-        // }
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // --- Test 4 times ---
+        for (int i = 0; i < 4; i++) {
+            appTest.addCustomer(customers);
+            System.out.println("\n");
+        }
+
+        System.out.println("\n---")
     }
 }
